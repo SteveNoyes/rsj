73e6b9757f4174652e7269bd6e131ec87176108c
SteveNoyes <stevenmnoyes.af@gmail.com>
2022-09-27 20:40:50 -0400

    fill murray

---
+++ script.js
@@ -0,0 +1,37 @@
+const ONCE = 'string';
+let num = 1; 
+let bool = true;
+let arr = [1, 2, 3, 4, 5];
+let obj = {
+  name: 'onelle',
+  age: 42,
+  email: 'anything@gmail.com',
+  password: 'abcdefg123'
+};
+
+
+/* =================================
+  javascript basics
+
+  comments
+
+  variables
+
+  math: + - * / %
+
+
+
+==================================== */
+
+
+// while(a < 5) {
+// console.log('hello steven');
+// a ++;
+// }
+
+
+// let a = 1;
+// do {
+//   console.log('this is fun');
+// } while (a < 7);
+// a++;
